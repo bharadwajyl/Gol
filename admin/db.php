5c99e467466b4d4d724c63f4d1073b809c59b1f6
Mahesh <100456710+bharadwajyl@users.noreply.github.com>
2023-04-13 02:15:45 +0000

    Update db.php

--- admin/db.php
+++ admin/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "phpmyadmin", "Loosers@2016", "phpmyadmin");
+$conn = new mysqli("localhost", "u_name", "pssd", "db");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
