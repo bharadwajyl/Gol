5d197ed8b048be11ded56c8e47d1be2892afc4e4
Mahesh <100456710+bharadwajyl@users.noreply.github.com>
2023-04-19 05:32:05 +0000

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -16,9 +16,18 @@ $(".team_container_icon").on("click", function(){
     $(this).addClass("active");
     let class_named = parseInt($(this).attr('class').split(' ')[1].replace("slide_", "")) - 1;
     $.get("api.json", function(content) { 
-        $(".team_container_content_title").html(content[class_named].title);
-        $(".team_container_content_sub_title").html(content[class_named].sub_title);
-        $(".team_container_content_desc").html(content[class_named].desc);
+        $(".team_container_content_title").html(content["team"][class_named].title);
+        $(".team_container_content_sub_title").html(content["team"][class_named].subtitle);
+        $(".team_container_content_desc").html(content["team"][class_named].desc);
+        $(".team_image").attr("src", content["team"][class_named].image);
+        $(".team_icon_"+class_named).attr("src", content["team"][class_named].icon);
+        let splitter = content["team"][class_named][0].social_icons.split(", ");
+        let url = content["team"][class_named][0].social_icons_url.split(", ");
+        let social_icons = "";
+        for (let i=0; i<splitter.length; i++){
+            social_icons += "<a href='"+ url[i] +"'><i class='fa fa-"+ splitter[i] +"'></i></a>";
+        }
+        $(".team_social_icons").html(social_icons);
     });
 });
 
